68c9e3454df2e30763d4db687140e3f3453c579b
Ernest Cheung <ErnestCheung02@gmail.com>
2020-12-01 00:13:39 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+	return (
+		<div>
+			<h1> SFPOPOS </h1>
+			<div className="Title"> San Francisco Privately Owned Public Spaces</div>
+		</div>
+	);
+}
+export default Title;
