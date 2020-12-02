1a81185b507d61f8e0d07548c9f421aafdbf026a
Ernest Cheung <ErnestCheung02@gmail.com>
2020-12-01 19:12:12 -0800

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+	return (
+		<div className="Footer">
+			<header> Ernest Cheung 2020 </header>
+		</div>
+	);
+}
+
+export default Footer;
