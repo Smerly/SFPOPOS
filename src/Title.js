8d52e9e17aa582b2c857e4fb3bba70e70dcd6559
Ernest Cheung <ErnestCheung02@gmail.com>
2020-12-02 01:57:41 -0800

    Finished 6th step

--- src/Title.js
+++ src/Title.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Title.css';
+import { NavLink } from 'react-router-dom';
 
 function Title() {
 	return (
 		<div>
 			<h1> SFPOPOS </h1>
 			<div className="Title"> San Francisco Privately Owned Public Spaces</div>
+			<NavLink
+				className="nav-link"
+				activeClassName="nav-link-active"
+				exact
+				to="/"
+			>
+				List
+			</NavLink>
+
+			<NavLink
+				className="nav-link"
+				activeClassName="nav-link-active"
+				to="/about"
+			>
+				About
+			</NavLink>
 		</div>
 	);
 }
